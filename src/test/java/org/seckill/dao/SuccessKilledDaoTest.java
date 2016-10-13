2a3dd903f05060dfd110764bbeb47e6a82011920
gusy39 <gusy39@163.com>
2016-10-13 23:55:12 +0800

    modify 2

---
+++ src/test/java/org/seckill/dao/SuccessKilledDaoTest.java
@@ -0,0 +1,38 @@
+package org.seckill.dao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.seckill.entity.SuccessKilled;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Administrator on 2016/10/13.
+ */
+
+//配置spring和junit整合，junit启动时加载spring容器
+@RunWith(SpringJUnit4ClassRunner.class)
+//告诉junit spring配置文件
+@ContextConfiguration({"classpath:spring/spring-dao.xml"})
+public class SuccessKilledDaoTest {
+    @Resource
+    private SuccessKilledDao successKilledDao;
+    @Test
+    public void testInsertSuccessKilled() throws Exception {
+        long id=1000L;
+        long phone=13556899635L;
+        int i = successKilledDao.insertSuccessKilled(id, phone);
+        System.out.println(i);
+    }
+
+    @Test
+    public void testQueryByIdWithSeckill() throws Exception {
+        SuccessKilled successKilled = successKilledDao.queryByIdWithSeckill(1000L,13556899635L);
+        System.out.println(successKilled);
+
+    }
+}
